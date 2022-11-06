700b682c6e74fff04362b8f570b581ba9e5b8bba
Victor José Costa Farias <47372055+Petrucchio@users.noreply.github.com>
2022-11-06 17:15:59 -0300

    API

---
+++ ModuloAPI/ModuloAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ModuloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+            return Ok(obj);
+        }
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"olá {nome}, sejá bem vindo!";
+            return Ok(new { mensagem });
+        }
+        
+    }
+}
